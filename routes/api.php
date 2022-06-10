bf5ae8293796ef0cf37b844bafa0cde9bf032636
achawayne3031 <59153600+achawayne3031@users.noreply.github.com>
2022-06-10 05:52:30 +0100

    'update'

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(
     [
         'namespace' => 'App\Http\Controllers',
-        'middleware' => 'cors',
+        ///  'middleware' => 'cors',
     ],
     function () {
         Route::post('/admin-login', 'AdminController@admin_login');

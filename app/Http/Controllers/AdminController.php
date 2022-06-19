dfbbf498d74e418a8dfb81d7f168489448b35a31
achawayne3031 <59153600+achawayne3031@users.noreply.github.com>
2022-06-19 11:39:19 +0100

    'update'

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -175,7 +175,7 @@ class AdminController extends Controller
         $total_vendors = Users::where('role', '3')->count();
         $total_dealers = Users::where('role', '4')->count();
         $total_products = Products::count();
-        // $total_order = Cart::where('status', '1')->count();
+        $total_order = Cart::where('status', '1')->count();
 
         $logged_vendors = Users::where('role', '3')
             ->where('last_login', '!=', null)
@@ -202,7 +202,7 @@ class AdminController extends Controller
         $this->result->data->total_vendors = $total_vendors;
         $this->result->data->total_dealers = $total_dealers;
         $this->result->data->total_products = $total_products;
-        // $this->result->data->total_order = $total_order;
+        $this->result->data->total_order = $total_order;
 
         $this->result->message = 'Admin Dashboard Data';
         return response()->json($this->result);

7bfe57ad27c4b920ad570f493f94e2273e1de1c5
Tripletens <tripletens.kc@gmail.com>
2022-07-08 13:50:32 +0100

    add the user_details for each report

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1979,7 +1979,12 @@ class AdminController extends Controller
 
     public function get_all_reports()
     {
-        $reports = Report::all();
+        $reports = Report::join('users', 'users.id', '=', 'reports.user_id')
+        ->select('reports.*','users.full_name','users.first_name','users.last_name',
+            'users.email','users.role','users.role_name','users.dealer_name','users.vendor_name')
+        ->orderBy('reports.id','desc')
+        ->get();
+
         $this->result->status = true;
         $this->result->status_code = 200;
         $this->result->message = 'All reports fetched successfully';

6313246b965aeb50bbbd26bdefd07d07a9dd05af
achawayne3031 <59153600+achawayne3031@users.noreply.github.com>
2022-07-12 15:35:53 +0100

    update

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -66,6 +66,8 @@ class ChatController extends Controller
                 $sender_data->first_name . ' ' . $sender_data->last_name;
             $value->to_username =
                 $receiver_data->first_name . ' ' . $receiver_data->last_name;
+
+            $value->time_ago = ChatController::timeAgo($value->created_at);
         }
 
         $this->result->status = true;
@@ -121,6 +123,74 @@ class ChatController extends Controller
             $this->result->message = 'Chat Stored Successfully';
 
             return response()->json($this->result);
+        }
+    }
+
+    //Function definition
+
+    public static function timeAgo($time_ago)
+    {
+        $time_ago = strtotime($time_ago);
+        $cur_time = time();
+        $time_elapsed = $cur_time - $time_ago;
+        $seconds = $time_elapsed;
+        $minutes = round($time_elapsed / 60);
+        $hours = round($time_elapsed / 3600);
+        $days = round($time_elapsed / 86400);
+        $weeks = round($time_elapsed / 604800);
+        $months = round($time_elapsed / 2600640);
+        $years = round($time_elapsed / 31207680);
+        // Seconds
+        if ($seconds <= 60) {
+            return 'just now';
+        }
+        //Minutes
+        elseif ($minutes <= 60) {
+            if ($minutes == 1) {
+                return 'one minute ago';
+            } else {
+                return "$minutes minutes ago";
+            }
+        }
+        //Hours
+        elseif ($hours <= 24) {
+            if ($hours == 1) {
+                return 'an hour ago';
+            } else {
+                return "$hours hrs ago";
+            }
+        }
+        //Days
+        elseif ($days <= 7) {
+            if ($days == 1) {
+                return 'yesterday';
+            } else {
+                return "$days days ago";
+            }
+        }
+        //Weeks
+        elseif ($weeks <= 4.3) {
+            if ($weeks == 1) {
+                return 'a week ago';
+            } else {
+                return "$weeks weeks ago";
+            }
+        }
+        //Months
+        elseif ($months <= 12) {
+            if ($months == 1) {
+                return 'a month ago';
+            } else {
+                return "$months months ago";
+            }
+        }
+        //Years
+        else {
+            if ($years == 1) {
+                return 'one year ago';
+            } else {
+                return "$years years ago";
+            }
         }
     }
 }

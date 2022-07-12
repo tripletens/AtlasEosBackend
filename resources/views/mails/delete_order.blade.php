3e2242a908994b6913854027a07747693a900c37
Kalu Chinonso <tripletens.kc@gmail.com>
2022-07-12 12:32:40 +0100

    add the delete order email

---
+++ resources/views/mails/delete_order.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.mail')
+
+@section('content')
+
+<div class="container-fluid">
+    <span class="alert alert-info text-start">
+        <h3>Order Deletion Notification</h3> <br/>
+        <p>Sorry your order has been deleted successfully. </p> <br/>
+        <p><b>Order Details: </b> </p>
+    </span>
+</div>
+
+@endsection

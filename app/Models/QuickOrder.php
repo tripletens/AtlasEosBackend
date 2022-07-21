ce2cc46fb329226bab2686178ded15dffcc1c073
Kalu Chinonso <tripletens.kc@gmail.com>
2022-07-21 18:19:09 +0100

    fixed the fetch quickorder by atlas_id and vendor no

--- app/Models/QuickOrder.php
+++ app/Models/QuickOrder.php
@@ -11,5 +11,5 @@ class QuickOrder extends Model
     use HasFactory, SoftDeletes;
     // set the table
     protected $table = "quick_order";
-    protected $fillable = ['id', 'uid', 'dealer', 'vendor', 'atlas_id', 'product_id', 'qty','price','unit_price', 'status', 'created_at', 'updated_at','deleted_at'];
+    protected $fillable = ['id', 'uid', 'dealer', 'vendor', 'atlas_id', 'product_id', 'groupings', 'qty', 'price', 'unit_price', 'vendor_no', 'status', 'created_at', 'updated_at', 'deleted_at'];
 }

8f38901bf9cd3ed579296f804b64ca4cb3b453d6
Kalu Chinonso <tripletens.kc@gmail.com>
2022-07-22 22:30:51 +0100

    Update SpecialOrderController.php

--- app/Http/Controllers/SpecialOrderController.php
+++ app/Http/Controllers/SpecialOrderController.php
@@ -175,17 +175,18 @@ class SpecialOrderController extends Controller
     // fetch special order by uid
     public function fetch_special_order_by_dealer_id($dealer_id)
     {
-        $check_special_order = SpecialOrder::join('vendors', 'vendors.id', '=', 'special_orders.vendor_id')
+        $check_special_order = SpecialOrder::where('special_orders.dealer_id', $dealer_id)
+//                 join('vendors', 'vendors.id', '=', 'special_orders.vendor_id')
 //             ->join('users', 'users.account_id', '=', 'special_orders.dealer_id')
-                ->where('special_orders.dealer_id', $dealer_id)
+             
                 ->select(
-                'vendors.vendor_code as vendor_code',
-                'vendors.vendor_name as vendor_name',
-                'vendors.role as vendor_role',
-                'vendors.role_name as vendor_role_name',
-                'vendors.status as vendor_role_name',
-                'vendors.created_at as vendor_created_at',
-                'vendors.updated_at as vendor_updated_at',
+//                 'vendors.vendor_code as vendor_code',
+//                 'vendors.vendor_name as vendor_name',
+//                 'vendors.role as vendor_role',
+//                 'vendors.role_name as vendor_role_name',
+//                 'vendors.status as vendor_role_name',
+//                 'vendors.created_at as vendor_created_at',
+//                 'vendors.updated_at as vendor_updated_at',
                 'special_orders.*',
 //                 'users.*'
             )

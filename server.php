9c4719ea049741610104896439780a9e01576c22
Kalu Chinonso <tripletens.kc@gmail.com>
2022-08-07 07:50:55 +0100

    Update server.php

--- server.php
+++ server.php
@@ -11,6 +11,10 @@ $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
 
+$command = "php artisan schedule:work"; 
+$path = base_path();
+exec("cd {$path} && {$command}");
+
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
 // application without having installed a "real" web server software here.

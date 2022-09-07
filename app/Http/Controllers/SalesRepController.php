0c5f7f879322f05728613feabe5f1fa4fff436d0
Kalu Chinonso <tripletens.kc@gmail.com>
2022-09-07 21:55:01 +0100

    add the chart

--- app/Http/Controllers/SalesRepController.php
+++ app/Http/Controllers/SalesRepController.php
@@ -358,8 +358,7 @@ class SalesRepController extends Controller
             foreach ($user_privileged_dealers_array as $user_privilaged_dealer) {
                 $user_privileged_dealers_format = str_replace('"', '', $user_privilaged_dealer);
 
-                $get_priviledged_dealer_details = Users::where('account_id', $user_privileged_dealers_format)
-                    ->select('id', 'account_id', 'full_name', 'first_name', 'last_name', 'vendor_name', 'company_name')->get();
+                $get_priviledged_dealer_details = Dealer::where('dealer_code', $user_privileged_dealers_format)->get();
 
                 if (count($get_priviledged_dealer_details) > 0) {
                     // yay its an array

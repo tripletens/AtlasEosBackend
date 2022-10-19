fcd51fa53af62b8bb1260a791a776dbccbc898a9
Kalu Chinonso <tripletens.kc@gmail.com>
2022-10-19 21:24:44 +0100

    revert purchaser summary back

--- routes/api.php
+++ routes/api.php
@@ -1128,7 +1128,7 @@ Route::group(
 
         // fetch product summary by dealer_id
         Route::get(
-            '/product-summary/{uid}',
+            '/product-summary/{account_id}',
             'SummaryController@product_summary'
         );
 

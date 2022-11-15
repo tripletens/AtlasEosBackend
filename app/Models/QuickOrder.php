80313d8e3543299091c86b034bd532f615dc66f9
achawayne3031 <59153600+achawayne3031@users.noreply.github.com>
2022-11-14 17:06:13 -0700

    updadate

--- app/Models/QuickOrder.php
+++ app/Models/QuickOrder.php
@@ -10,6 +10,23 @@ class QuickOrder extends Model
 {
     use HasFactory, SoftDeletes;
     // set the table
-    protected $table = "quick_order";
-    protected $fillable = ['id', 'uid', 'dealer', 'vendor', 'atlas_id', 'product_id', 'groupings', 'qty', 'price', 'unit_price', 'vendor_no', 'status', 'created_at', 'updated_at', 'deleted_at'];
+    protected $table = 'quick_order';
+    protected $fillable = [
+        'id',
+        'uid',
+        'dealer',
+        'vendor',
+        'atlas_id',
+        'xref',
+        'product_id',
+        'groupings',
+        'qty',
+        'price',
+        'unit_price',
+        'vendor_no',
+        'status',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
 }

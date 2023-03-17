974cb2d1cab0a6c23ddb895f7cd34d2a57419f43
achawayne3031 <59153600+achawayne3031@users.noreply.github.com>
2023-03-17 01:43:37 +0100

    upftae

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -937,7 +937,7 @@ class AdminController extends Controller
     {
         $cart = Cart::where('status', '1')
             ->orderBy('xref', 'asc')
-            ->paginate(500);
+            ->paginate(1000);
 
         $cart_data = $cart->items();
 

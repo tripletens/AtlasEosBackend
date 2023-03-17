35193df646298425af18a483583ffa65ad90c8b1
achawayne3031 <59153600+achawayne3031@users.noreply.github.com>
2023-03-17 02:08:58 +0100

    update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -937,7 +937,7 @@ class AdminController extends Controller
     {
         $cart = Cart::where('status', '1')
             ->orderBy('xref', 'asc')
-            ->paginate(2200);
+            ->paginate(2000);
 
         $cart_data = $cart->items();
 

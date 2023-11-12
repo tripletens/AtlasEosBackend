272574a5b08a7e39c336845711a5dc825a767ebd
achawayne3031 <59153600+achawayne3031@users.noreply.github.com>
2023-11-12 02:04:55 +0100

    update

--- app/Http/Controllers/DealerController.php
+++ app/Http/Controllers/DealerController.php
@@ -997,6 +997,8 @@ class DealerController extends Controller
                 $status = $item->status;
                 $groupings = $item->groupings;
                 $xref = $item->xref;
+                $type = $item->type;
+                $booking = $item->booking;
 
                 if (
                     Cart::where('dealer', $dealer)
@@ -1028,6 +1030,8 @@ class DealerController extends Controller
                         'unit_price' => $unit_price,
                         'status' => $status,
                         'xref' => $xref,
+                        'type' => $type,
+                        'booking' => $booking,
                     ]);
 
                     if (!$save) {
@@ -3189,7 +3193,7 @@ class DealerController extends Controller
                     $this->result->status = true;
                     $this->result->status_code = 404;
                     $this->result->message = 'item has been added already';
-                    break;
+                    // break;
                 } else {
                     $save = Cart::create([
                         'uid' => $uid,
@@ -3202,7 +3206,7 @@ class DealerController extends Controller
                         'price' => trim($price),
                         'unit_price' => trim($unit_price),
                         'status' => $status,
-                        'type' => $type,
+                        'type' => $item->type,
                         'booking' => $booking,
                     ]);
 
